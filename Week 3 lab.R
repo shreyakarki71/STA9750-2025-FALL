f65636fc8a1eb0a95fcde790169c1bb9d14aa2fa
Shreya Karki <karkishreya12@gmail.com>
2025-09-29 11:38:16 -0400

    Update MP01 report with revised press releases

---
+++ Week 3 lab.R
@@ -0,0 +1,15 @@
+1 + 2 + 3 + 4 + 5
+3** (2 *5 - 1) / 24**5
+1/ # this will cause an error
+  7
+1 # this will work 
+\7
+# all function calls have a fundamental syntax
+cos(pi)
+sqrt(4)
+?R.Version
+?cos # question mark function name will tell you about what does the function do
+cos
+lm # type a name without () to see its implementation 
+args(lm) # cuts all other bs and gives just few lines of implementation 
+
